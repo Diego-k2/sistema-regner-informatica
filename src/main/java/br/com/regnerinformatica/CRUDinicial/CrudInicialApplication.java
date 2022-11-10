ea565a46fb5615de3bf9b26bf00f2af7207342ff
Diego-k2 <degosantosiva@gmail.com>
2022-11-10 11:57:24 -0300

    Commit inicial

---
+++ src/main/java/br/com/regnerinformatica/CRUDinicial/CrudInicialApplication.java
@@ -0,0 +1,13 @@
+package br.com.regnerinformatica.CRUDinicial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudInicialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudInicialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/regnerinformatica/CRUDinicial/CrudInicialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.regnerinformatica.CRUDinicial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudInicialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
